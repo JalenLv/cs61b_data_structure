b94b316789c9b3bcadd505e445d7606fa82c1b38
JalenLv <sejalenlv@mail.scut.edu.cn>
2024-09-23 14:43:35 +0800

    completed lec2.2 guide exercises

--- lectureCode-sp18/lists2/IntList.java
+++ lectureCode-sp18/lists2/IntList.java
@@ -1,44 +1,104 @@
 public class IntList {
-	public int first;
-	public IntList rest;
+  public int first;
+  public IntList rest;
 
-	public IntList(int f, IntList r) {
-		first = f;
-		rest = r;
-	}
+  public IntList(int f, IntList r) {
+    first = f;
+    rest = r;
+  }
 
-	/** Return the size of the list using... recursion! */
-	public int size() {
-		if (rest == null) {
-			return 1;
-		}
-		return 1 + this.rest.size();
-	}
+  /**
+   * Return the size of the list using... recursion!
+   */
+  public int size() {
+    if (rest == null) {
+      return 1;
+    }
+    return 1 + this.rest.size();
+  }
 
-	/** Return the size of the list using no recursion! */
-	public int iterativeSize() {
-		IntList p = this;
-		int totalSize = 0;
-		while (p != null) {
-			totalSize += 1;
-			p = p.rest;
-		}
-		return totalSize;
-	}
+  /**
+   * Return the size of the list using no recursion!
+   */
+  public int iterativeSize() {
+    IntList p = this;
+    int totalSize = 0;
+    while (p != null) {
+      totalSize += 1;
+      p = p.rest;
+    }
+    return totalSize;
+  }
 
-	/** Returns the ith item of this IntList. */
-	public int get(int i) {
-		if (i == 0) {
-			return first;
-		}
-		return rest.get(i - 1);
-	}
+  /**
+   * Returns the ith item of this IntList.
+   */
+  public int get(int i) {
+    if (i == 0) {
+      return first;
+    }
+    return rest.get(i - 1);
+  }
 
-	public static void main(String[] args) {
-		IntList L = new IntList(15, null);
-		L = new IntList(10, L);
-		L = new IntList(5, L);
+  public void addAdjacent() {
+    IntList p = this;
+    while (p.rest != null) {
+      if (p.first == p.rest.first) {
+        p.first *= 2;
+        p.rest = p.rest.rest;
+      } else {
+        p = p.rest;
+      }
+    }
+  }
 
-		System.out.println(L.get(100));
-	}
-} 
+  public void print() {
+    IntList p = this;
+    while (p != null) {
+      System.out.print(p.first + " ");
+      p = p.rest;
+    }
+    System.out.println();
+  }
+
+  public void squareAdd(int x) {
+    IntList p = this;
+    while (true) {
+      if (p.rest == null) {
+        p.rest = new IntList(p.first * p.first, null);
+        p.rest.rest = new IntList(x, null);
+        break;
+      } else {
+        IntList tmp = new IntList(p.first * p.first, p.rest);
+        p.rest = tmp;
+        p = tmp.rest;
+      }
+    }
+  }
+
+  public static void main(String[] args) {
+    IntList L = new IntList(15, null);
+    L = new IntList(10, L);
+    L = new IntList(5, L);
+
+    System.out.println(L.get(1));
+
+    L = new IntList(3, null);
+    L = new IntList(2, L);
+    L = new IntList(1, L);
+    L = new IntList(1, L);
+
+    L.addAdjacent();
+    L.print();
+
+    L = new IntList(2, null);
+    L = new IntList(1, L);
+    L.print();
+
+    L.squareAdd(5);
+    L.print();
+
+    L.squareAdd(7);
+    L.print();
+  }
+}
--- lectureCode-sp18/lists2/SLList.java
+++ lectureCode-sp18/lists2/SLList.java
@@ -1,72 +1,108 @@
- /** An SLList is a list of integers, which hides the terrible truth
-   * of the nakedness within. */
-public class SLList {	
-	private static class IntNode {
-		public int item;
-		public IntNode next;
+/**
+ * An SLList is a list of integers, which hides the terrible truth
+ * of the nakedness within.
+ */
+public class SLList {
+  private static class IntNode {
+    public int item;
+    public IntNode next;
 
-		public IntNode(int i, IntNode n) {
-			item = i;
-			next = n;
-			System.out.println(size);
-		}
-	} 
+    public IntNode(int i, IntNode n) {
+      item = i;
+      next = n;
+    }
+  }
 
-	/* The first item (if it exists) is at sentinel.next. */
-	private IntNode sentinel;
-	private int size;
+  /* The first item (if it exists) is at sentinel.next. */
+  private IntNode sentinel;
+  private int size;
 
-	private static void lectureQuestion() {
-		SLList L = new SLList();
-		IntNode n = IntNode(5, null);
-	}
+  /**
+   * Creates an empty SLList.
+   */
+  public SLList() {
+    sentinel = new IntNode(63, null);
+    size = 0;
+  }
 
-	/** Creates an empty SLList. */
-	public SLList() {
-		sentinel = new IntNode(63, null);
-		size = 0;
-	}
+  /**
+   * Creates an SLList with x as the first item.
+   */
+  public SLList(int x) {
+    sentinel = new IntNode(63, null);
+    sentinel.next = new IntNode(x, null);
+    size = 1;
+  }
 
-	public SLList(int x) {
-		sentinel = new IntNode(63, null);
-		sentinel.next = new IntNode(x, null);
-		size = 1;
-	}
+  /**
+   * Creates an SLList with an array of integers.
+   */
+  public SLList(int[] x) {
+    size = x.length;
+    sentinel = new IntNode(63, null);
 
- 	/** Adds x to the front of the list. */
- 	public void addFirst(int x) {
- 		sentinel.next = new IntNode(x, sentinel.next);
- 		size = size + 1;
- 	}
+    IntNode p = sentinel;
+    for (int i = 0; i < x.length; i++) {
+      p.next = new IntNode(x[i], null);
+      p = p.next;
+    }
+  }
 
- 	/** Returns the first item in the list. */
- 	public int getFirst() {
- 		return sentinel.next.item;
- 	}
+  /**
+   * Adds x to the front of the list.
+   */
+  public void addFirst(int x) {
+    sentinel.next = new IntNode(x, sentinel.next);
+    size = size + 1;
+  }
 
- 	/** Adds x to the end of the list. */
- 	public void addLast(int x) {
- 		size = size + 1; 		
+  /**
+   * Returns the first item in the list.
+   */
+  public int getFirst() {
+    return sentinel.next.item;
+  }
 
- 		IntNode p = sentinel;
+  /**
+   * Delete the first item in the list.
+   */
+  public void deleteFirst() {
+    size -= 1;
+    if (sentinel.next != null)
+      sentinel.next = sentinel.next.next;
+  }
 
- 		/* Advance p to the end of the list. */
- 		while (p.next != null) {
- 			p = p.next;
- 		}
+  /**
+   * Adds x to the end of the list.
+   */
+  public void addLast(int x) {
+    size = size + 1;
 
- 		p.next = new IntNode(x, null);
- 	}
- 	
- 	/** Returns the size of the list. */
- 	public int size() {
- 		return size;
- 	}
+    IntNode p = sentinel;
 
-	public static void main(String[] args) {
- 		/* Creates a list of one integer, namely 10 */
- 		SLList L = new SLList();
- 		L.addLast(20);
- 		System.out.println(L.size());
- 	}
+    /* Advance p to the end of the list. */
+    while (p.next != null) {
+      p = p.next;
+    }
+
+    p.next = new IntNode(x, null);
+  }
+
+  /**
+   * Returns the size of the list.
+   */
+  public int size() {
+    return size;
+  }
+
+  public static void main(String[] args) {
+    /* Creates a list of one integer, namely 10 */
+    SLList L = new SLList();
+    L.addLast(20);
+    System.out.println(L.size());
+
+    int[] a = new int[]{0, 1, 2, 3, 4};
+    SLList array_L = new SLList(a);
+    System.out.println(array_L.getFirst());
+  }
 }
